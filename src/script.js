759e6966f38748cf0cbd00f049e4bc16f81a8cc3
“melonono” <melody.vikstrom@gmail.com>
2020-05-15 02:46:31 +0200

    slick slider in header styled with indexSCSS

--- src/script.js
+++ src/script.js
@@ -1,16 +1,35 @@
-
-// $(document).ready(function(){
-//     $('.your-class').slick({
-//       setting-name: setting-value
-//     });
-//   });
-
+// header slick slider
 $('.slickslider').slick({
 
     slidesToScroll: 1,
     arrows: false,
     dots: true,
     vertical: true,
-    verticalSwiping: true
+    verticalSwiping: true,
         
 });
+
+// News section slick slider
+// $('.news-slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+
+
+//   smoothe scroll
+
+$('.navigation__link').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    )
+  });
+  
+
